4dae8a117df5eb6d3684d3224747395bf497307d
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2020-04-14 19:08:12 -0700

    Move old python stuff

--- DataStructures/LinkedList/python/linked_lists.py
+++ DataStructures/LinkedList/python/linked_lists.py
@@ -9,15 +9,18 @@ from typing import TypeVar, Type
 T = TypeVar("T")
 TLinkedListSNode = TypeVar("TLinkedListSNode", bound="LinkedListSNode")
 
+
 class LinkedListSNode(object):
     """Singly linked list node"""
 
     def __init__(self, data: T = None, next_node: TLinkedListSNode = None):
         self.data = data
         self.next_node = next_node
+
+
 class LinkedListS(object):
     """Singly linked list"""
-    
+
     def __init__(self, initializer_data: T = None) -> None:
         self.head = None
         self.tail = None
@@ -36,16 +39,16 @@ class LinkedListS(object):
         else:
             self.tail.next_node = new_node
             self.tail = new_node
-    
+
     def delete(self, deletion_data: T, occurances: int = None) -> bool:
         """Delete specified occurances of nodes containing `deletion_data`.
 
         Deletion order is in order of occurance starting from head.
 
         Args:
-          deletion_data: [T], the data contained by the node(s) to be removed 
+          deletion_data: [T], the data contained by the node(s) to be removed
               from the list
-          occurances: [int], number of occurances to remove, starting from the 
+          occurances: [int], number of occurances to remove, starting from the
               head of the list. If `None`, removes all occurances from list
 
         Returns:
@@ -66,14 +69,14 @@ class LinkedListS(object):
                     self.head = self.head.next_node
                 else:
                     curr_node.next_node = curr_node.next_node.next_node
-                    
+
             curr_node = curr_node.next_node
 
         return False
 
     def search(self, search_data: T) -> LinkedListSNode:
         """Finds node with `search_data` in list. Returns None if not found.
-        
+
         Performs linear search starting from head of list.
         """
 
@@ -92,6 +95,5 @@ class LinkedListS(object):
         while curr_node:
             print("{} -> ".format(curr_node.data), end="")
             curr_node = curr_node.next_node
-        
-        print()
 
+        print()
