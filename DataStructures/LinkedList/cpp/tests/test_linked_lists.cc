850d9c991c052ba1bcf100403026ef15604be3f4
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2020-05-01 12:21:54 -0700

    Reorg tests

--- DataStructures/LinkedList/cpp/tests/test_linked_lists.cc
+++ DataStructures/LinkedList/cpp/tests/test_linked_lists.cc
@@ -7,54 +7,59 @@
 namespace linkedlists {
 class SLinkedListTestInt : public ::testing::Test {
  protected:
-  void SetUp() override { list_ = std::make_unique<SLinkedList<int>>(); }
-  void TearDown() override {
-    list_.reset();
+  void SetUp() override {
+    emptyList_ = std::make_unique<SLinkedList<int>>();
+    smallList_ = std::make_unique<SLinkedList<int>>();
+    bigList_ = std::make_unique<SLinkedList<int>>();
+    smallList_->insert(3);
+    smallList_->insert(5);
+    smallList_->insert(-4);
+    bigList_->insert(10);
+    bigList_->insert(43);
+    bigList_->insert(5000);
+    bigList_->insert(10);
+    bigList_->insert(-35);
   }
-  std::unique_ptr<SLinkedList<int>> list_;
+  void TearDown() override {
+    emptyList_.reset();
+    smallList_.reset();
+    bigList_.reset();
+  }
+  std::unique_ptr<SLinkedList<int>> emptyList_;
+  std::unique_ptr<SLinkedList<int>> smallList_;
+  std::unique_ptr<SLinkedList<int>> bigList_;
 };
+
 TEST_F(SLinkedListTestInt, InsertValidInt) {
-  EXPECT_TRUE(list_->insert(3));
-  EXPECT_TRUE(list_->insert(5));
-  EXPECT_TRUE(list_->insert(-4));
-  EXPECT_TRUE(list_->insert(500));
+  EXPECT_TRUE(emptyList_->insert(3));
+  EXPECT_TRUE(emptyList_->insert(5));
+  EXPECT_TRUE(emptyList_->insert(-4));
+  EXPECT_TRUE(emptyList_->insert(500));
   std::string printout = "3->5->-4->500->";
-  EXPECT_EQ(printout, list_->toString());
+  EXPECT_EQ(printout, emptyList_->toString());
 }
 
 TEST_F(SLinkedListTestInt, ListSize) {
-  EXPECT_TRUE(list_->insert(3));
-  EXPECT_TRUE(list_->insert(5));
-  EXPECT_TRUE(list_->insert(-4));
-  EXPECT_EQ(3, list_->size);
+  EXPECT_EQ(3, smallList_->size);
 }
 
 TEST_F(SLinkedListTestInt, FindExisting) {
-  list_->insert(10);
-  list_->insert(43);
-  list_->insert(5000);
-  list_->insert(10);
-  list_->insert(-35);
-  SLinkedListNode<int> *expectedNode = list_->head;
-  EXPECT_EQ(expectedNode, list_->find(10));
-  expectedNode = list_->head->next;
-  EXPECT_EQ(expectedNode, list_->find(43));
-  expectedNode = list_->head->next->next;
-  EXPECT_EQ(expectedNode, list_->find(5000));
-  expectedNode = list_->head;
-  EXPECT_EQ(expectedNode, list_->find(10));
-  expectedNode = list_->head->next->next->next;
-  EXPECT_NE(expectedNode, list_->find(10));
-  EXPECT_EQ(*expectedNode, *list_->find(10));
-  expectedNode = list_->tail;
-  EXPECT_EQ(expectedNode, list_->find(-35));
+  SLinkedListNode<int> *expectedNode = bigList_->head;
+  EXPECT_EQ(expectedNode, bigList_->find(10));
+  expectedNode = bigList_->head->next;
+  EXPECT_EQ(expectedNode, bigList_->find(43));
+  expectedNode = bigList_->head->next->next;
+  EXPECT_EQ(expectedNode, bigList_->find(5000));
+  expectedNode = bigList_->head;
+  EXPECT_EQ(expectedNode, bigList_->find(10));
+  expectedNode = bigList_->head->next->next->next;
+  EXPECT_NE(expectedNode, bigList_->find(10));
+  EXPECT_EQ(*expectedNode, *bigList_->find(10));
+  expectedNode = bigList_->tail;
+  EXPECT_EQ(expectedNode, bigList_->find(-35));
 }
 
 TEST_F(SLinkedListTestInt, FindNotExisting) {
-  list_->insert(10);
-  list_->insert(43);
-  list_->insert(5000);
-  list_->insert(-35);
-  EXPECT_EQ(nullptr, list_->find(9001));
+  EXPECT_EQ(nullptr, bigList_->find(9001));
 }
 }  // namespace linkedlists
